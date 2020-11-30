7f51d412e0170bb6d9798336b8b4e1e04ad40c34
hrushigade <71587685+hrushigade@users.noreply.github.com>
2020-11-29 19:10:47 -0800

    Add files via upload
    checks whether number is palindrome or not

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number"))
+temp=n
+rev=0
+while(n>0):
+	dig=n%10
+	rev=rev*10+dig
+	n=n//10
+if(temp==rev):
+	print("the number is palindrome")
+else:
+	print("the number is not palindrome") 
