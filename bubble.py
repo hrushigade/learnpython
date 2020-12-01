722179732d64842157ccf2b6ede13a0a9fefb9e2
hrushigade <71587685+hrushigade@users.noreply.github.com>
2020-11-30 17:12:32 -0800

    Add files via upload
    bubble sort

---
+++ bubble.py
@@ -0,0 +1,10 @@
+def bubblesort(nlist):
+	for passnum in range(len[nlist]-1,0,-1):
+		for i in range(passsnum):
+			if nlist[i]>nlist[i+1]:
+				temp=nlist[i]
+				nlist[i]=nlist[i+1]
+				nlist[i+1]=temp
+nlist=[14,46,43,27,51,41]
+bubblesort(nlist)
+print(nlist)
